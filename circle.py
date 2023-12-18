7ddd1b1de8534e43eb30bff38982d2c2eca00cfa
MARSSIII <Satanicsevensins@mail.ru>
2023-12-18 20:25:10 +0300

    tests: added tests in circle.py, rectangle.py, square.py and triangle.py

--- circle.py
+++ circle.py
@@ -1,24 +1,86 @@
 import math
+import unittest
+
+
+class CircleTestCase(unittest.TestCase):
+
+    """
+    Class of tests for Trinagle.py func
+    :param: unittest (TestCase) parent class
+    """
+
+    def test_Circle_AreaOfZero(self):
+
+        """
+        case:
+        Test func(Area) for zero values
+        """
+
+        self.assertRaises(ValueError, area, 0)
+
+    def test_Circle_AreaIntValue(self):
+
+        """
+        case:
+        Test func(Area) for int values
+        """
+
+        result = area(5)
+        self.assertEquals(result, math.pi * 5 * 5)
+
+    def test_Circle_AreaOfNegative(self):
+
+        """
+        case:
+        Test func(Area) for Negative values
+        """
+
+        self.assertRaises(ValueError, area, -3)
+
+    def test_Circle_PerimeterOfZero(self):
+
+        """
+        case:
+        Test func(Perimeter) for Zero values
+        """
+
+        self.assertRaises(ValueError, perimeter, 0)
+
+    def test_Circle_PerimeterIntValues(self):
+
+        """
+        case:
+        Test func(Perimeter) for int values
+        """
+
+        result = perimeter(5)
+        self.assertEquals(result, 10 * math.pi)
+
+    def test_Circle_PerimeterOfNegative(self):
+
+        """
+        case:
+        Test func(perimeter) for negative values
+        """
+
+        self.assertRaises(ValueError, perimeter, -3)
 
 
 def area(r):
 
-    '''
-    Принимает число r (радиус окружности), возвращает произведение числа ПИ на r в квадрате (площадь окружности)
-    :param r : радиус окружности.
-    :return: площадь окружности, рассчитаное по формуле ПИ * r * r.
-    '''
+    """
+    :param r: circle radius
+    :return: Circle Area(Formula: Pi * radius ^ 2
+    """
 
     return math.pi * r * r
 
 
 def perimeter(r):
 
-    '''
-    Принимает число r (радиус окружности), возвращает произведение числа 2, числа ПИ и числа r (длина окружности).
-    :param r: радиус окружности.
-    :return: длина окружности, рассчитаное по формуле ПИ * 2 * r.
-    '''
+    """
+    :param r: circle radius
+    :return: Circle Perimeter(Formula: 2 * Pi * radius
+    """
 
     return 2 * math.pi * r
-
--- rectangle.py
+++ rectangle.py
@@ -1,21 +1,87 @@
+import unittest
+
+
+class RectTestCase(unittest.TestCase):
+
+    """
+    Class of tests for Rectangle.py func
+    :param: unittest (TestCase) parent class
+    """
+
+    def test_Rectangle_AreaOfZero(self):
+
+        """
+        case:
+        Test func(Area) for zero values
+        """
+
+        self.assertRaises(ValueError, area, 0, 1)
+
+    def test_Rectangle_AreaIntValue(self):
+
+        """
+        case:
+        Test func(Area) for int values
+        """
+
+        result = area(2, 3)
+        self.assertEquals(result, 6)
+
+    def test_Rectangle_AreaOfNegative(self):
+
+        """
+        case:
+        Test func(Area) for Negative values
+        """
+
+        self.assertRaises(ValueError, area, -1, 34)
+
+    def test_Rectangle_PerimeterOfZero(self):
+
+        """
+        case:
+        Test func(Perimeter) for zero values
+        """
+
+        self.assertRaises(ValueError, perimeter, 3, 0)
+
+    def test_Rectangle_PerimeterIntValue(self):
+
+        """
+        case:
+        Test func(Perimeter) for int values
+        """
+
+        result = perimeter(5, 6)
+        self.assertEquals(result, 22)
+
+    def test_Rectangle_PerimeterNegative(self):
+
+        """
+        case:
+        Test func(Perimeter) for Negative values
+        """
+
+        self.assertRaises(ValueError, perimeter, -1, 3)
+
+
 def area(a, b):
 
-    '''
-    Принимает числа a, b (ширина прямоугольника, длина прямоугольника), возвращает произведение a и b (площадь прямоугольника)
-    :param a: ширина прямоугольника
-    :param b: длина прямоугольника
-    :return: произведение a (ширины) и b (длины) прямоугольника (площадь)
-    '''
+    """
+    :param a: side (first) of the rectangle
+    :param b: side (second) of the rectangle
+    :return: Rectangle Area (Formula: siae(first) * side(second))
+    """
 
     return a * b 
 
+
 def perimeter(a, b):
 
-    '''
-    Принимает числа a, b (ширина, длина прямоугольника), возвращает произведение суммы (a + b) и 2 (площадь)
-    :param a: шарина прямоугольника
-    :param b: длина прямоугольника
-    :return: Периметр прямоугольника ((ширина + длина) * 2)
-    '''
+    """
+    :param a: siae (first) of the rectangle
+    :param b: side (second) of the rectangle
+    :return: Rectangle Perimeter (Formula: (side(first) + side(second)) * 2)
+    """
 
     return (a + b) * 2 
